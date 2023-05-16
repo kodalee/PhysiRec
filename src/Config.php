c8763181f13d444465a4d8b0e1cc8af998dbd162
Koda Lee <kodaallen123@gmail.com>
2023-05-16 11:23:54 -0700

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -4,12 +4,12 @@ namespace Physler;
 
 class Config {
     const GA_CLIENT_ID = "62631792304-59mit2gahkh43094prti6p493vnf3gad.apps.googleusercontent.com";
-    const GA_CLIENT_SECRET = "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";
+    const GA_CLIENT_SECRET = "";
 
     const EDU_EMAIL_SUFFIX = "gecs.org";
 
     const MYSQL_ADDRESS = "127.0.0.1:3306";
     const MYSQL_USERNAME = "physler";
-    const MYSQL_PASSWORD = "kodabear12";
+    const MYSQL_PASSWORD = "password";
     const MYSQL_DATABASE = "physler_app";
 }
