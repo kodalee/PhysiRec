2d14fa8ebf35296c75469951b54dea9a8182a34f
Koda Lee <kodaallen123@gmail.com>
2023-05-18 07:13:17 -0700

    Read extended description
    - Password isn't used anymore (in a bunch of DB breaches)
    - Changed GA_CLIENT_SECRET & invalidated old key

--- src/Config.php
+++ src/Config.php
@@ -6,10 +6,10 @@ class Config {
     const GA_CLIENT_ID = "62631792304-59mit2gahkh43094prti6p493vnf3gad.apps.googleusercontent.com";
     const GA_CLIENT_SECRET = "";
 
-    const EDU_EMAIL_SUFFIX = "gecs.org";
+    const EDU_EMAIL_SUFFIX = "";
 
     const MYSQL_ADDRESS = "127.0.0.1:3306";
-    const MYSQL_USERNAME = "physler";
-    const MYSQL_PASSWORD = "password";
-    const MYSQL_DATABASE = "physler_app";
+    const MYSQL_USERNAME = "";
+    const MYSQL_PASSWORD = "";
+    const MYSQL_DATABASE = "";
 }
