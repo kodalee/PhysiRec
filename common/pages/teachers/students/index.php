3717d72300cfeed268671937f8b8f2ac77604567
Koda Lee <kodaallen123@gmail.com>
2023-05-23 09:12:41 -0700

    Update index.php

--- common/pages/teachers/students/index.php
+++ common/pages/teachers/students/index.php
@@ -8,12 +8,12 @@ $user = SessionVisitor::GetActive()->GetVisitorUser();
 
 <div class="mx-2 p-2 mt-3">
     <div>
-        <h1 class="h2 mb-0 text-center">Students</h1>
+        <h1 class="h2 mb-0">Students</h1>
     </div>
 </div>
 <div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
+    <div class="row">
+        <div class="col-md-6 ">
             <div class="card bg-blur">
                 <div class="card-header">
                     <h5 class="mt-1 mb-0">Your students</h4>
@@ -36,6 +36,20 @@ $user = SessionVisitor::GetActive()->GetVisitorUser();
                                 </div>
                             </a>
                         <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 ">
+            <div class="card bg-blur">
+                <div class="card-header text-center">
+                    <h5 class="my-1">Add student</h4>
+                    <div>Add a student by entering their email address.</div>
+                </div>
+                <div class="card-body">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Email address" aria-label="Email address" aria-describedby="addBtn">
+                        <button class="btn btn-primary" type="button" id="addBtn">Add</button>
                     </div>
                 </div>
             </div>
