72d9d33f0b0b5fb18d4db0d2619c5e7e31449576
Koda Lee <kodaallen123@gmail.com>
2023-05-23 09:06:08 -0700

    Update index.php

--- common/pages/teachers/students/index.php
+++ common/pages/teachers/students/index.php
@@ -12,8 +12,8 @@ $user = SessionVisitor::GetActive()->GetVisitorUser();
     </div>
 </div>
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-md-6 ">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
             <div class="card bg-blur">
                 <div class="card-header">
                     <h5 class="mt-1 mb-0">Your students</h4>
@@ -36,22 +36,6 @@ $user = SessionVisitor::GetActive()->GetVisitorUser();
                                 </div>
                             </a>
                         <?php } ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 ">
-            <div class="card bg-blur">
-                <div class="card-header text-center">
-                    <h5 class="my-1">My heartbeat</h4>
-                </div>
-                <div class="card-body">
-                    <div class="buttons">
-                        <button class="btn-danger btn w-100" data-ajax="activity/bpmlogger" interactive>Log my current heart
-                            beat</button>
-                    </div>
-                    <div class="bpm-history">
-                        place date with bpm and scenario (188 bpm after running a mile on june 1st 2025)
                     </div>
                 </div>
             </div>
